661aaa66ae8909ff23ee4e38ab0af8a2875d308d
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-18 01:38:10 -0800

    👷 Define constants

--- scripts/BurnTokens.ts
+++ scripts/BurnTokens.ts
@@ -6,3 +6,6 @@ import { baseSepolia, sepolia } from "viem/chains";
 import { abi } from "../artifacts/contracts/ArcadeToken.sol/ArcadeToken.json";
 import * as dotenv from "dotenv";
 dotenv.config();
+
+const providerApiKey = process.env.ALCHEMY_API_KEY || "";
+const minterPrivateKey = process.env.PRIVATE_KEY || "";
