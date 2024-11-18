55c525410310357139806aeb46815f50b83adf4e
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-17 22:36:41 -0800

    📜 Add function validateParameters

--- scripts/MintTokens.ts
+++ scripts/MintTokens.ts
@@ -10,3 +10,26 @@ dotenv.config();
 
 const providerApiKey = process.env.ALCHEMY_API_KEY || "";
 const minterPrivateKey = process.env.PRIVATE_KEY || "";
+
+function validateParameters(parameters: string[]) {
+    if (!parameters || parameters.length < 4)
+      throw new Error("Parameters not provided");
+  
+    const contractAddress = parameters[0] as `0x${string}`;
+    if (!contractAddress) throw new Error("Contract address not provided");
+    if (!/^0x[a-fA-F0-9]{40}$/.test(contractAddress))
+      throw new Error("Invalid contract address");
+  
+    const toAddress = parameters[1] as `0x${string}`;
+    if (!toAddress) throw new Error("To address not provided");
+    if (!/^0x[a-fA-F0-9]{40}$/.test(toAddress))
+      throw new Error("Invalid to address");
+  
+    const amount = parameters[2];
+    if (isNaN(Number(amount))) throw new Error("Invalid amount");
+  
+    const network = parameters[3];
+    if (!network) throw new Error("Network not provided");
+  
+    return { contractAddress, toAddress, amount, network };
+  }
