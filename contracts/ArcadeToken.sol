0765dd57ed91670e9ac89dbaff6089c29ce156b3
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-17 21:57:39 -0800

    📝 Add event Wager

--- contracts/ArcadeToken.sol
+++ contracts/ArcadeToken.sol
@@ -6,6 +6,8 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract ArcadeToken is ERC20, ERC20Burnable, AccessControl {
+    event Wager(address indexed owner, uint256 indexed amount);
+
     
 
 }
