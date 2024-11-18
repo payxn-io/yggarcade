65c99e8dded6dff85e7841156dba99d4cac7b8b5
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-18 01:41:01 -0800

    👷 Create TokenStatus script and add imports

--- scripts/TokenStatus.ts
+++ scripts/TokenStatus.ts
@@ -1,3 +1,4 @@
 // npx ts-node --files ./scripts/TokenStatus.ts CONTRACT_ADDRESS ACCOUNT_ADDRESS NETWORK
 
 import { createPublicClient, http } from "viem";
+import { baseSepolia, sepolia } from "viem/chains";
