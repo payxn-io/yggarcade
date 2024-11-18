6de05608aa6799b62df42fe4708b26d1760bb4d1
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-18 01:21:58 -0800

    🎨 Modified header frontend

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -94,7 +94,7 @@ export const Header = () => {
           </div>
           <div className="flex flex-col">
             <span className="font-bold leading-tight">YGG Arcade</span>
-            <span className="text-xs">Payxn dev stack</span>
+            <span className="text-xs">Payxn Game Dev</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
