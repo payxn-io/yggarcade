dce6ab467535a3dd9cbc9d39c90f1cedb4471be4
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-18 01:23:27 -0800

    🎨 Modified title & desc in layout frontend

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -5,8 +5,8 @@ import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
-  title: "Scaffold-ETH 2 App",
-  description: "Built with 🏗 Scaffold-ETH 2",
+  title: "YGG Arcade - Payxn",
+  description: "Play with your favourite Arcade Games",
 });
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
