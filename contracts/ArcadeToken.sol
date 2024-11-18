04a08bdcc58a0b28af31c8e1050f18be3c7fdf68
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-17 21:57:05 -0800

    📝 Create ArcadeToken contract

--- contracts/ArcadeToken.sol
+++ contracts/ArcadeToken.sol
@@ -4,3 +4,8 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract ArcadeToken is ERC20, ERC20Burnable, AccessControl {
+    
+
+}
