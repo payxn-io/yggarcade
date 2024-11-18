6033437fde0f6633c1da11d7c02eac6205b516f6
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-17 22:15:31 -0800

    📜 Import viem and dotenv

--- scripts/DeployArcadeToken.ts
+++ scripts/DeployArcadeToken.ts
@@ -1,3 +1,9 @@
 // npx ts-node --files ./scripts/DeployArcadeToken.ts TOKEN_NAME TOKEN_SYMBOL NETWORK
 
 import { createPublicClient, http, createWalletClient, formatEther } from "viem";
+
+import { privateKeyToAccount } from "viem/accounts";
+import { sepolia, baseSepolia } from "viem/chains";
+import { abi, bytecode } from "../artifacts/contracts/ArcadeToken.sol/ArcadeToken.json";
+import * as dotenv from "dotenv";
+dotenv.config();
