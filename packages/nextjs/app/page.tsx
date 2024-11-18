30d4ce5d86bbd504f0d987e6138c05b8b18832d8
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-18 03:13:14 -0800

    🎨 Fixing styling details

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -45,7 +45,7 @@ const Home: NextPage = () => {
           </p>
         </div>
 
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
+        <div className="flex-grow bg-base-300 w-full mt-1 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
             <div className="relative flex w-[401px] h-[300px]">
@@ -107,7 +107,7 @@ const Home: NextPage = () => {
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
             <div className="relative flex w-[401px] h-[300px]">
               <Image
-                src="/yggarcade-undead.png"
+                src="/yggarcade-math.png"
                 alt="Math is mathing"
                 fill
                 style={{ objectFit: "contain" }}
