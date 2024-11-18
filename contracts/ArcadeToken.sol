77565c1fd4b210a7477886ece7a097ed51e59bd2
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-17 21:55:22 -0800

    📝 Create ArcadeToken contract

---
+++ contracts/ArcadeToken.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract ArcadeToken is ERC20, ERC20Burnable, AccessControl {
+    event Wager(address indexed owner, uint256 indexed amount);
+
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function wager(uint256 value) public virtual {
+        _burn(_msgSender(), value);
+        emit Bridge(msg.sender, value);
+    }
+
+}
