ad10ead47c63a0ea37ac541cea7bfe91d1121a58
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-18 03:13:32 -0800

    🎨 Fixing styling details

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
           <h1 className="text-center">
-            <span className="block text-2xl mb-2">Welcome to</span>
+            <span className="block text-1xl mb-2">Welcome to</span>
             <span className="block text-4xl font-bold">YGG Arcade</span>
           </h1>
           
