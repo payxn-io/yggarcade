2c3eb0d6c30f802d9c9d7141055ae3ad808ee818
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-18 01:42:40 -0800

    👷 Add function validateParameters

--- scripts/TokenStatus.ts
+++ scripts/TokenStatus.ts
@@ -8,3 +8,22 @@ dotenv.config();
 
 const providerApiKey = process.env.ALCHEMY_API_KEY || "";
 
+function validateParameters(parameters: string[]) {
+    if (!parameters || parameters.length < 3)
+      throw new Error("Parameters not provided");
+  
+    const contractAddress = parameters[0] as `0x${string}`;
+    if (!contractAddress) throw new Error("Contract address not provided");
+    if (!/^0x[a-fA-F0-9]{40}$/.test(contractAddress))
+      throw new Error("Invalid contract address");
+  
+    const accountAddress = parameters[1] as `0x${string}`;
+    if (!accountAddress) throw new Error("Account address not provided");
+    if (!/^0x[a-fA-F0-9]{40}$/.test(accountAddress))
+      throw new Error("Invalid account address");
+  
+    const network = parameters[2];
+    if (!network) throw new Error("Network not provided");
+  
+    return { contractAddress, accountAddress, network };
+}
