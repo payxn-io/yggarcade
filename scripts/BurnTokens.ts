a1d66f549a65490d835bf49a4e5c5f3a7e83d708
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-18 01:37:09 -0800

    👷 Create BurnTokens script and add imports

--- scripts/BurnTokens.ts
+++ scripts/BurnTokens.ts
@@ -3,3 +3,4 @@
 import { createPublicClient, http, createWalletClient } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import { baseSepolia, sepolia } from "viem/chains";
+import { abi } from "../artifacts/contracts/ArcadeToken.sol/ArcadeToken.json";
