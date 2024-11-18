776b27a59e5b27c518819a6552c5c9fc7dddc501
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-18 01:38:30 -0800

    👷 Add function validateParameters

--- scripts/BurnTokens.ts
+++ scripts/BurnTokens.ts
@@ -9,3 +9,21 @@ dotenv.config();
 
 const providerApiKey = process.env.ALCHEMY_API_KEY || "";
 const minterPrivateKey = process.env.PRIVATE_KEY || "";
+
+function validateParameters(parameters: string[]) {
+    if (!parameters || parameters.length < 3)
+      throw new Error("Parameters not provided");
+  
+    const contractAddress = parameters[0] as `0x${string}`;
+    if (!contractAddress) throw new Error("Contract address not provided");
+    if (!/^0x[a-fA-F0-9]{40}$/.test(contractAddress))
+      throw new Error("Invalid contract address");
+  
+    const amount = parameters[1];
+    if (isNaN(Number(amount))) throw new Error("Invalid amount");
+  
+    const network = parameters[2];
+    if (!network) throw new Error("Network not provided");
+  
+    return { contractAddress, amount, network };
+  }
