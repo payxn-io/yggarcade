44afb2203215315ac171b47c95573c50bd7ed1a3
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-17 22:34:31 -0800

    📜 Create MintTokens.ts and add imports

---
+++ scripts/MintTokens.ts
@@ -0,0 +1,9 @@
+// npx ts-node --files ./scripts/MintTokens.ts CONTRACT_ADDRESS TO_ADDRESS AMOUNT NETWORK
+
+import { createPublicClient, http, createWalletClient } from "viem";
+
+import { privateKeyToAccount } from "viem/accounts";
+import { baseSepolia, sepolia } from "viem/chains";
+import { abi } from "../artifacts/contracts/ArcadeToken.sol/ArcadeToken.json";
+import * as dotenv from "dotenv";
+dotenv.config();
