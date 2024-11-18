85309e913f29bdc6f12ffd7839718a7d6ead4caf
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-18 01:36:30 -0800

    👷 Create BurnTokens script and add imports

--- scripts/BurnTokens.ts
+++ scripts/BurnTokens.ts
@@ -1,3 +1,4 @@
 // npx ts-node --files ./scripts/BurnTokens.ts CONTRACT_ADDRESS AMOUNT NETWORK
 
 import { createPublicClient, http, createWalletClient } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
