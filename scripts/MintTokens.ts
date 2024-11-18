96dbad70cfcbe3d92f5401edaa01ace110dabd95
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-17 22:37:17 -0800

    📜 Add async function main

--- scripts/MintTokens.ts
+++ scripts/MintTokens.ts
@@ -33,3 +33,51 @@ function validateParameters(parameters: string[]) {
   
     return { contractAddress, toAddress, amount, network };
   }
+
+  async function main() {
+    console.log("\n");
+    const { contractAddress, toAddress, amount, network } = validateParameters(process.argv.slice(2));
+  
+    const chain = network === "base" ? baseSepolia : sepolia;
+    const subdomain = network === "base" ? "base-sepolia" : "eth-sepolia";
+    const transport = http(`https://${subdomain}.g.alchemy.com/v2/${providerApiKey}`);
+  
+    const account = privateKeyToAccount(`0x${minterPrivateKey}`);
+    const minter = createWalletClient({
+      account,
+      chain: chain,
+      transport: transport,
+    });
+  
+    console.log(`Minting ${amount} tokens to account: ${toAddress}`);
+    console.log("Confirm? (Y/n)");
+  
+    const stdin = process.stdin;
+    stdin.on("data", async function (d) {
+      if (d.toString().trim() == "Y") {
+        const hash = await minter.writeContract({
+          address: contractAddress,
+          abi,
+          functionName: "mint",
+          args: [toAddress, BigInt(amount)],
+        });
+        console.log("Transaction hash:", hash);
+        console.log("Waiting for confirmations...");
+        const publicClient = createPublicClient({
+          chain: chain,
+          transport: transport,
+        });
+        const receipt = await publicClient.waitForTransactionReceipt({ hash });
+        console.log(`Transaction confirmed: ${receipt.status}`);
+        console.log(`Block: ${receipt.blockNumber}`)
+      } else {
+        console.log("Operation cancelled");
+      }
+      process.exit();
+    });
+  }
+
+  main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
