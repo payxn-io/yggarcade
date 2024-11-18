c277486aaa01275c980c482efa3a0a858298ffd5
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-18 05:18:38 -0800

    👷 Created a new undead app component

---
+++ packages/nextjs/app/undead/page.tsx
@@ -0,0 +1,13 @@
+import ResponsiveIframe from './_components/ResponsiveIframe';
+
+export default function Home() {
+  return (
+    <div>
+      {/*<h1>Responsive Iframe in Next.js with TypeScript</h1>*/}
+      <ResponsiveIframe 
+        src="https://payxn.xyz/yggarcade/undead/" 
+        title="Ammo Arcade"
+      />
+    </div>
+  );
+}
