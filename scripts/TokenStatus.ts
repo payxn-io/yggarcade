fa27f94a107e2e21c810298d5e5b77038e920dcf
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-18 01:41:40 -0800

    👷 Create TokenStatus script and add imports

--- scripts/TokenStatus.ts
+++ scripts/TokenStatus.ts
@@ -3,3 +3,4 @@
 import { createPublicClient, http } from "viem";
 import { baseSepolia, sepolia } from "viem/chains";
 import { abi } from "../artifacts/contracts/ArcadeToken.sol/ArcadeToken.json";
+import * as dotenv from "dotenv";
