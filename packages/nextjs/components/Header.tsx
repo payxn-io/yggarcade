1cfea873f19c6bdd1a8dff231abcd60b05ec4290
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-18 00:06:16 -0800

    🎨 Updated YGG header logo

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -93,8 +93,8 @@ export const Header = () => {
             <Image alt="Payxn logo" className="cursor-pointer" fill src="/payxn-500.png" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">YGG Arcade</span>
+            <span className="text-xs">Payxn dev stack</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
